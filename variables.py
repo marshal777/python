14c1c6e20d750a59745e00ab87385aa526405bf9
marshal777 <marshal7358@gmail.com>
2024-10-31 16:44:27 +0530

    first commit

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Variable Names
+john = 25  # john's age is 25 years old
+
+# A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume). Rules for Python variables:
+a = 5
+carname = "pourchasr"
+
+# A variable name must start with a letter or the underscore character.
+# A variable name cannot start with a number
+
+#2my_name = "luna" # is an error
+
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Z = 1  
+z = 1   # Variable names are case-sensitive (myvar and MyVar are two different variables)
+
+# Variable names are case-sensitive (age, Age, and AGE are three different variables).
+mycat = "Luna"  # This is valid because "mycat" does not conflict with any Python keyword or function.
+
+# A variable name cannot be any of the Python keywords.
+print("Hello World")  # This is correct because "print" is a command in Python for outputting text to the console.
+
+# Incorrect example
+# Print("Hello World")  # This is incorrect because "Print" with an uppercase "P" is not recognized as a command in Python.
+
+# Correct example
+print("Hello World")  # This is correct because "print" is a recognized function in Python.
